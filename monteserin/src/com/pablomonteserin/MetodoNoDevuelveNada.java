a98b744fbf3ecf2e604a2d5e4f43bbd3c3efafae
Sr. Monteserin <pablomonteserin@pablomonteserin.com>
2018-04-12 19:53:43 +0200

    subida

---
+++ monteserin/src/com/pablomonteserin/MetodoNoDevuelveNada.java
@@ -0,0 +1,14 @@
+package com.pablomonteserin;
+
+public class MetodoNoDevuelveNada {
+
+	public static void saludar(){
+		System.out.println("Buenas tardes!");
+	}
+
+	public static void main(String[] args) {
+		
+		saludar();
+	}
+
+}
