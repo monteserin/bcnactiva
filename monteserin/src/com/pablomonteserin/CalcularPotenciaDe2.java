fafe5044470d8abe1283ac72bfd5a296fb3f15db
Sr. Monteserin <pablomonteserin@pablomonteserin.com>
2018-04-12 19:08:55 +0200

    subida

---
+++ monteserin/src/com/pablomonteserin/CalcularPotenciaDe2.java
@@ -0,0 +1,19 @@
+package com.pablomonteserin;
+
+public class CalcularPotenciaDe2 {
+
+	public static void main(String[] args) {
+		int n = 2;
+		int r = 0;
+		
+		for(int i=0; i<3; i++) {
+			r =r*n;
+		}
+		
+		System.out.println(r);
+		
+		
+
+	}
+
+}
