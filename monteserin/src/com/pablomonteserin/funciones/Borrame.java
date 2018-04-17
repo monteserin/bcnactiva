3b0d0553dd75a27b3d357a25046dcad02e324017
Sr. Monteserin <pablomonteserin@pablomonteserin.com>
2018-04-17 19:58:46 +0200

    subida

---
+++ monteserin/src/com/pablomonteserin/funciones/Borrame.java
@@ -0,0 +1,13 @@
+package com.pablomonteserin.funciones;
+
+public class Borrame {
+	
+	int edad = 5;
+	public static void main(String[] args) {
+		Borrame b = new Borrame();
+		
+		b.edad++;
+	}
+
+	
+}
