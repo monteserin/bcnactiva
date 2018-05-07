702a0a83881bec023696661b5610166a1dfa9fd6
Sr. Monteserin <pablomonteserin@pablomonteserin.com>
2018-05-07 15:24:34 +0200

    mapa del tesoro

---
+++ monteserin/src/com/pablomonteserin/depositosConstructor/Main.java
@@ -0,0 +1,18 @@
+package com.pablomonteserin.depositosConstructor;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Deposito d = new Deposito(10,15);
+		
+		d.setLargo(20);
+	//	d.largo = 60;
+		
+	}
+
+}
